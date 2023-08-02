f675821a5f115d6c55f97c2f29ddd455cdfc6dd0
DomSpicer <dominic.spicer@gmail.com>
2023-08-02 14:57:58 +0100

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import { api } from "~/utils/api";
 const Home: NextPage = () => {
   const user = useUser();
 
-  const { data } = api.example.getAll.useQuery();;
+  const { data } = api.posts.getAll.useQuery();;
 
   return (
     <>
@@ -22,6 +22,11 @@ const Home: NextPage = () => {
           {!!user.isSignedIn && <SignOutButton />}
         </div>
         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <div>
+          {data?.map((post) => (
+          <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
